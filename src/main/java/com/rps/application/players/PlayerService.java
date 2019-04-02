16ad55b077e05ab57c1e64aadaa2211cf78be3d0
Suyash <suyash@codenomads.nl>
2019-04-02 18:45:29 +0200

    redundant code

--- src/main/java/com/rps/application/players/PlayerService.java
+++ src/main/java/com/rps/application/players/PlayerService.java
@@ -14,7 +14,6 @@ import org.springframework.stereotype.Service;
 @Service
 public class PlayerService {
 
-  @Autowired
   private PlayersInMemoryRepository playersInMemoryRepository;
 
   public PlayerService(PlayersInMemoryRepository playersInMemoryRepository) {
